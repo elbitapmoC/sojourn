4f1f81601f08c1f53e496030252a5c3f232d0dde
Ahkal <bp@sojourn.llc>
2022-07-06 12:23:45 -0400

    accomplishments and capabilities removed.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
-import Acomplishments from "../components/Acomplishments/Acomplishments";
 import Hero from "../components/Hero/Hero";
 import Projects from "../components/Projects/Projects";
-import Capabilities from "../components/Capabilities/Capabilities";
 import Testimonials from "../components/Testimonials/Testimonials";
 import { Layout } from "../layout/Layout";
 import CTA from "../components/CTA/CTA";
